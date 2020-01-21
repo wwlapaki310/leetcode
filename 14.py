6908a979843bb14c1258a124b9c72f77639c763b
satoru akita <wwlap24@gmail.com>
2020-01-22 00:26:33 +0900

    0122寝る前
    あ

---
+++ 14.py
@@ -0,0 +1,13 @@
+import math
+import numpy as np
+import os
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        print(strs)
+
+def hello():
+    print("hello")
+
+if __name__== "__main__":
+    s=list(input())
